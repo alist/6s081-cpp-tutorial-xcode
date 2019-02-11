2cb0e7e4519b7d35b95aad4f9b53291212caa9e9
Alexander List <atlex2@gmail.com>
2019-02-11 11:36:05 -0500

    3:Cast to delete correctly?

--- 6s081 cpp/main.cpp
+++ 6s081 cpp/main.cpp
@@ -14,10 +14,16 @@ int main(int argc, const char * argv[]) {
   //std::cout << "Hello, World!\n";
   
   /// Not the c++ 11 way
-  BaseClass * object = new ChildClass();
+  /// Note 1: this makes sure we're converting using parent class ABI -- this case,
+  /// using explicitly definined conversions or using constructor
+  BaseClass * object = static_cast<BaseClass*>(new ChildClass());
   std::cout << object->nonVirtualFavoriteString() << std::endl;
   std::cout << object->virtualFavoriteString() << std::endl;
-  delete object;
+  /// Note 2: static_cast downcast would work HERE, but wouldn't in all cases
+  ///         i.e. ChildClass * childCastedObject = static_cast<ChildClass*>(object);
+  /// Note 3: Dynamic cast is preferred (not to 6.031 paradigms), but requires ≥ 1 virtual method in BaseClass
+  ChildClass * childCastedObject = dynamic_cast<ChildClass*>(object);
+  delete childCastedObject;
   
   return 0;
 }
