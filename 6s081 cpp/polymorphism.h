b176fa7f1b2988fc0aa48576ad8189ccffa6e684
Alexander List <atlex2@gmail.com>
2019-02-11 12:10:50 -0500

    7: shared ptrs not born

--- 6s081 cpp/polymorphism.h
+++ 6s081 cpp/polymorphism.h
@@ -20,8 +20,7 @@ class BaseClass {
     return "BaseClass virtualFavoriteString";
   }
   
-  // NOTE **: Removing virtuality for a sec
-  ~BaseClass(){
+  virtual ~BaseClass(){
     std::cout << "deleting BaseClass \n";
   }
 };
@@ -40,16 +39,21 @@ class LifetimeTestee {
 class ChildClass : public BaseClass {
  /// reps should be private anyway..
  private:
-  /// Note A: Behold, unique_ptr, it only allows one
+  /// Note A: Behold, unique_ptr, it only allows one (owner).
+  ///         i.e. std::unique_ptr<std::vector<int>> myData = associatedData; leaves
+  ///         associatedData without any internal pointer.
+  ///         unique only has move constructor-- red EffModC++ pp49-51 for constructor basics.
   /// Note B: According to https://en.cppreference.com/w/cpp/language/default_initialization
   ///         class types like (smart pointers) are default initialized,
   ///         but that does not mean their internals are too!!
   std::unique_ptr<std::vector<int>> associatedData;
-  LifetimeTestee testee;
+  /// Note 1: AND Behold shared_ptr, it supports the copy and move constructor.
+  ///         it deletes the underlying object when no copies of the shared_ptr exist anymore
+  /// Note 2: If you run now, you'll see it is not "BORN".
+  std::shared_ptr<LifetimeTestee> testeePtr;
   
  public:
   ChildClass() {
-    //     associatedData->push_back(1); would fail until we do the next line...
     associatedData = std::unique_ptr<std::vector<int>>(new std::vector<int>{});
   }
   
