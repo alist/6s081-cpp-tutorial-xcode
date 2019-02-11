cd4a2f06a6d0e9950f915e500e13605d8efcad9d
Alexander List <atlex2@gmail.com>
2019-02-11 10:47:23 -0500

    Initial Commit

---
+++ 6s081 cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  6s081 cpp
+//
+//  Created by Alexander H List on 2/11/19.
+//  Copyright © 2019 Alexander H List. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
